327be880596fad928864a6b8cfbd572b621c7849
Akuma Jamal <akumajamal8@gmail.com>
2023-07-18 20:21:50 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //program that calculates the difference between
+    //the year you are born and the current year
+    val current_year = 2023
+    val DOB = 2000
+    val diff = current_year - DOB
+    println("you are $diff years old")
+
+    //program to find the hypotenuse of a right sided
+    //triangle with base = 4cm and height = 3cm
+    val base = 4
+    val height = 3
+    val hypotenuse = (4*4) + (3*3)
+    println("the hypotenuse of the triangle is $hypotenuse"+"cm")
+}
